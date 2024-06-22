070377c39aaff85f2e61c7f64b25690f065ea930
Brian Gatundu <brianwgatundu@gmail.com>
2024-06-22 09:10:50 +0300

    Update BaseUrls.js

--- src/services/BaseUrls.js
+++ src/services/BaseUrls.js
@@ -4,8 +4,8 @@
 // export const profileApi = "http://127.0.0.1:8000/profile/";
 // export const charts = "http://127.0.0.1:8000/records/dailyrecord/";
 
-export const userUrl = "https://ginim-1c17f81fcc5.herokuapp.com/auth/";
+export const userUrl = "https://ginim-1cc17f81fcc5.herokuapp.com/auth/";
 export const cartBaseUrl = "https://ginim-1cc17f81fcc5.herokuapp.com/menu/";
-export const mpesaBaseUrl = "https://ginim-1c17f81fcc5.herokuapp.com/mpesa/";
-export const profileApi = "https://ginim-1c17f81fcc5.herokuapp.com/profile/";
-export const charts = "https://ginim-1c17f81fcc5.herokuapp.com/records/";
+export const mpesaBaseUrl = "https://ginim-1cc17f81fcc5.herokuapp.com/mpesa/";
+export const profileApi = "https://ginim-1cc17f81fcc5.herokuapp.com/profile/";
+export const charts = "https://ginim-1cc17f81fcc5.herokuapp.com/records/";
